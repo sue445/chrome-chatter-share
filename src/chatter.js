7496b021d4fbfeaa122736c51d70329600ba2667
sue445 <sue445@sue445.net>
2014-07-13 17:50:23 +0900

    Bad line breaking before '+'.

--- src/chatter.js
+++ src/chatter.js
@@ -67,10 +67,8 @@ var chatter = (function(){
 
     // private methods
     function getAuthorizeUrl(login_url, client_id, redirect_uri){
-        return login_url+'services/oauth2/authorize?display=page'
-            +'&response_type=token&client_id='+escape(client_id)
-            +'&redirect_uri='+escape(redirect_uri)
-            +'&state=' + state;
+        return login_url+'services/oauth2/authorize?display=page&response_type=token&client_id=' + escape(client_id) +
+            '&redirect_uri='+escape(redirect_uri) +'&state=' + state;
     }
 
     function createClient(){
