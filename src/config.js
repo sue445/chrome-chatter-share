021a252156c060cb24712c455891198a099d3278
sue445 <sue445@sue445.net>
2014-07-13 17:44:59 +0900

    write in dot notation.

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 var config= (function(){
     // public methods
     function getAccessToken(){
-        return localStorage["access_token"] || "";
+        return localStorage.access_token || "";
     }
 
     function getInstanceUrl(){
-        return localStorage["instance_url"] || "";
+        return localStorage.instance_url || "";
     }
 
     function setAccessToken(access_token){
-        localStorage["access_token"] = access_token;
+        localStorage.access_token = access_token;
     }
 
     function setInstanceUrl(instance_url){
-        localStorage["instance_url"] = instance_url;
+        localStorage.instance_url = instance_url;
     }
 
     function removeAuth(){
