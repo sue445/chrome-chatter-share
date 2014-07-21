7cbbae3c6f2f85abecbf3a58907dea594e01e9c0
sue445 <sue445@sue445.net>
2014-07-22 08:25:48 +0900

    Add Gruntfile skelton

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
+    grunt.registerTask('default', 'Log some stuff.', function() {
+        grunt.log.write('Logging some stuff...').ok();
+    });
+};
